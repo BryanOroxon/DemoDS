c648687278b94300a44d196cdf327584849a4c01
BryanOroxon <lanzer2002@hotmail.com>
2019-12-04 22:33:34 -0600

    Xaml Update

---
+++ DemoDS/DemoDS.Android/Dependency/ReverseGeocodeAndroid.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using Android.Locations;
+
+using Xamarin.Forms;
+
+using DemoDS.Models;
+using DemoDS.Interfaces;
+
+[assembly: Dependency(typeof(DemoDS.Droid.Dependency.ReverseGeocodeAndroid))]
+namespace DemoDS.Droid.Dependency
+{
+    public class ReverseGeocodeAndroid : IReverseGeocode
+    {
+        public async Task<LocationAddress> GetLocationAddress(double latitude, double longitude)
+        {
+            var geoCoder = new Geocoder(Android.App.Application.Context);
+            var addresses = await geoCoder.GetFromLocationAsync(latitude, longitude, 1);
+
+            if (addresses.Any())
+            {
+                var address = addresses.First();
+
+                var location = new LocationAddress()
+                {
+                    Name = address.FeatureName,
+                    City = address.Locality,
+                    Province = address.AdminArea,
+                    ZipCode = address.PostalCode,
+                    CountryCode=address.CountryCode.ToLower(),
+                    Country = $"{address.CountryName} ({address.CountryCode})",
+                    Address = address.MaxAddressLineIndex > 0 ? address.GetAddressLine(0) : string.Empty
+                };
+
+                return location;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DemoDS/DemoDS/Converters/ImagePathConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DemoDS.Converters
+{
+    public class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = "https://flagpedia.net/data/flags/w580/" + value.ToString() + ".png";
+            return path;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
